55695bbb67f411a25a4e3782475a905be1ad7605
staryskies <146684551+staryskies@users.noreply.github.com>
2025-09-24 11:40:11 -0700

    revive

--- api/index.js
+++
@@ -1,8 +0,0 @@
-// This file is used as an entry point for Vercel serverless functions
-// It simply re-exports the Express app from server.js
-
-// Import the Express app from server.js
-const app = require('../server');
-
-// Export the app for Vercel
-module.exports = app;
